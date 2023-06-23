8c62c4eb041d3bb02d131a93a364fc0b2589cef1
Evgenij <t89258000005@gmail.com>
2023-06-23 16:30:45 +0300

    first version

---
+++ public_html/Widget/script.js
@@ -0,0 +1,15 @@
+// import * as data from './yandex_reviews/company_reviews.json';
+// import data from "./yandex_reviews/company_reviews.json";
+
+// let reviews_number = document.querySelector('.reviews_numbers')
+// let reviews_value = document.querySelector('.nuber_star')
+
+// let reviews_name = document.querySelectorAll('.name_p')
+// let reviews_text = document.querySelectorAll('.reviews_text')
+// let reviews_ico = document.querySelectorAll('.icon_human')
+
+// reviews_number.innerHTML = '(' + data.company_info.company_count_reviews + ')'
+// reviews_value.innerHTML = data.company_info.company_count_stars
+
+// console.log(data.company_info.company_count_reviews)
+// console.log(data.company_info.company_count_stars)
